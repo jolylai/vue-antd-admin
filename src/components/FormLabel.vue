76f04a12bff8ba14ad5f185e370046d059111ad1
jolylai <jolylai163@163.com>
2022-08-19 18:03:43 +0800

    feat: menu

---
+++ src/components/FormLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-space>
+    <a-tooltip>
+      <template #title>
+        {{ tooltip }}
+      </template>
+      <QuestionCircleFilled />
+    </a-tooltip>
+    <slot />
+  </a-space>
+</template>
+
+<script lang="ts" setup>
+import { QuestionCircleFilled } from "@ant-design/icons-vue";
+
+export type FormLabelProps = {
+  tooltip?: string;
+};
+
+const props = defineProps<FormLabelProps>();
+</script>
