ee36b89828b91933a95db5abf074fe2401b2d607
jolylai <jolylai163@163.com>
2023-01-12 16:53:35 +0800

    feat: default layout

---
+++ src/stores/app.ts
@@ -0,0 +1,35 @@
+import { defineStore } from "pinia";
+import { merge } from "lodash";
+import { MenuSetting, ProjectConfig } from "types/config";
+
+interface AppState {
+  // darkMode?: ThemeEnum;
+  // Page loading status
+  // pageLoading: boolean;
+  // project config
+  projectConfig: ProjectConfig | null;
+  // When the window shrinks, remember some states, and restore these states when the window is restored
+  // beforeMiniInfo: BeforeMiniState;
+}
+
+export const useAppStore = defineStore("app", {
+  state: (): AppState => ({
+    projectConfig: {
+      menuSetting: { collapsed: false },
+    },
+  }),
+  getters: {
+    getProjectConfig(): ProjectConfig {
+      return this.projectConfig || ({} as ProjectConfig);
+    },
+    getMenuSetting(): MenuSetting {
+      return this.getProjectConfig.menuSetting;
+    },
+  },
+  actions: {
+    setProjectConfig(config: DeepPartial<ProjectConfig>): void {
+      this.projectConfig = merge(this.projectConfig || {}, config);
+      //  缓存
+    },
+  },
+});
