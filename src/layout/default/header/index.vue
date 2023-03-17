2e828298b8ef0cd18e25c73834fb7706c4682438
jolylai <jolylai163@163.com>
2023-03-17 16:04:59 +0800

    feta: layout

--- src/layout/default/header/index.vue
+++ src/layout/default/header/index.vue
@@ -4,11 +4,54 @@
       <img class="w-7 h-7" src="@/assets/vue.svg" />
       <h2 class="text-white text-lg m-0">Vue Antd Admin</h2>
     </a-space>
+
+    <div>
+      <a-dropdown>
+        <a-space class="ant-dropdown-link" @click.prevent>
+          <a-avatar
+            src="https://cy-picgo.oss-cn-hangzhou.aliyuncs.com/healthy-beef-burger.jpg"
+          />
+          <span>Tom</span>
+        </a-space>
+        <template #overlay>
+          <a-menu>
+            <a-menu-item>
+              <a-space>
+                <UserOutlined />
+                <span>个人中心</span>
+              </a-space>
+            </a-menu-item>
+            <a-menu-item>
+              <a-space>
+                <SettingOutlined />
+                <span>个人设置</span>
+              </a-space>
+            </a-menu-item>
+            <a-menu-item>
+              <a-space>
+                <LogoutOutlined />
+                <span>退出登录</span>
+              </a-space>
+            </a-menu-item>
+          </a-menu>
+        </template>
+      </a-dropdown>
+    </div>
   </a-layout-header>
 </template>
 
+<script lang="ts" setup>
+import {
+  UserOutlined,
+  SettingOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons-vue";
+</script>
+
 <style scoped>
 .layout-header {
+  display: flex;
+  justify-content: space-between;
   height: 48px;
   line-height: 48px;
   color: #fff;
